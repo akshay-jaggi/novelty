10fa5deb5f0ebf728a5f07a38614ec3798f48759
Akshay Jaggi <akshay.x.jaggi@gmail.com>
2022-07-26 16:59:51 -0400

    Starting repo!

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Explorations of the role of novelty in data streaming deep learning settings',
+    author='Akshay Jaggi',
+    license='MIT',
+)
