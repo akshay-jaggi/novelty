bca64eec7e79f5d270b595bdb8018df9aea3a758
Akshay Jaggi <akshay.x.jaggi@gmail.com>
2022-08-26 21:27:21 -0400

    Major progress day

--- novelty/models/util.py
+++ novelty/models/util.py
@@ -1,3 +1,5 @@
+from tqdm import tqdm
+
 import torch
 import torch.nn.functional as F
 
@@ -39,7 +41,7 @@ def train(model, device, train_loader, optimizer):
 
 
 def train_streaming(model, device, train_loader, optimizer, epoch,
-                    batches_per_epoch = 100):
+                    batches_per_epoch = 100, step_sizes = []):
     model.train()
     train_loss = 0
     correct = torch.zeros(10)
@@ -49,6 +51,8 @@ def train_streaming(model, device, train_loader, optimizer, epoch,
             continue
         if batch_idx >= batches_per_epoch * epoch:
             break
+        if step_sizes:
+            optimizer.param_groups[0]['lr'] = step_sizes[target]
         train_loss, correct, kept = minibatch_step(model, device, optimizer, data, target, train_loss, kept, correct)
     return train_loss / kept.sum(), correct / kept, kept
 
@@ -70,6 +74,9 @@ def train_streaming_unbalanced(model, device, train_loader, optimizer, epoch,
             continue
         data = data[mask]
         target = target[mask]
+        if lr_scaler:
+            probs = experimental_dist(torch.arange(0, 10))
+            optimizer.param_groups[0]['lr'] = prob_func(probs[target]).item()
         train_loss, correct, kept = minibatch_step(model, device, optimizer, data, target, train_loss, kept, correct)
     return train_loss / kept.sum(), correct / kept, kept
 
@@ -87,3 +94,20 @@ def test(model, device, test_loader):
             correct = correct + itemwise_accuracy(pred, target)
             kept = kept + torch.bincount(target,minlength=10)
     return test_loss / kept.sum(), correct / kept
+
+
+def train_and_test_streaming(model, optimizer, train_loader, test_loader,
+                             epochs = 10, batches_per_epoch = 5000, step_sizes = [], device = 'cpu'):
+    kept_all = torch.zeros(10)
+    train_accs = []
+    test_accs = []
+
+    for epoch in tqdm(range(1, epochs + 1)):
+        train_loss, train_acc, kept = train_streaming(model, device, train_loader, optimizer, epoch,
+                                                      batches_per_epoch, step_sizes)
+        test_loss, test_acc = test(model, device, test_loader)
+        train_accs.append(train_acc)
+        test_accs.append(test_acc)
+        kept_all = kept_all + kept
+
+    return train_accs, test_accs, kept_all
